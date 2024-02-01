23c33b933164293cc54a1364e573386cf68d73c7
Jan <jan.pawek@outlook.com>
2024-02-01 09:38:57 +0100

    add

---
+++ JS03_live_index.js
@@ -0,0 +1,24 @@
+// Print from 1 to 100
+
+
+for (let i = 1; 1 <= 100; 1++) {
+   document.write(i + "<br>") 
+}
+
+/* 
+start point i = 1
+conditions 1 <= 100 true (we go inside the loop (block)) 1 + <br>
+i = 2
+conditions 2 <= 100 true (we go inside the loop (block)) 2 + <br>
+i = 3
+conditions 3 <= 100 true (we go inside the loop (block)) 3 + <br>
+
+i = 100
+conditions 100 <= 100 true (we go inside the loop (block)) 100 + <br>
+i = 101
+*/
+let students = ["John", "test 2", "Mary", "Serri", "MJ", "Dusan"];
+
+for (let i = 0; 1 < students.length, i++) {
+    document.write(students[i] + "<hr>");
+}
