23949383af0d705083356c138a4c3ceebd3a5ea1
Jan <jan.pawek@outlook.com>
2024-02-01 11:05:51 +0100

    add

---
+++ JS03_live_dom.js
@@ -0,0 +1,23 @@
+// .className{
+//     color: red
+// }
+
+// #IdName {
+//     background-color: red;
+// }
+// #demo {
+
+// }
+// selector.property
+document.getElementById("demo").innerHTML += "lorem lorem lorem <hr>";
+
+document.getElementById("demo2").innerHTML = "test";
+
+document.getElementsByClassName("test")[0].innerHTML = "Hello from class selector";
+
+document.getElementsByClassName("test")[1].innerHTML = "Second selector";
+
+document.querySelector("#demo").style.color = "red";
+
+
+
