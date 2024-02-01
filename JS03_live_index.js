a783746b6183c06bbd95e5e013b8d0b9d50f1c03
Jan <jan.pawek@outlook.com>
2024-02-01 10:17:23 +0100

    add

--- JS03_live_index.js
+++ JS03_live_index.js
@@ -1,9 +1,9 @@
 // Print from 1 to 100
+// fehler hafter Code
 
-
-for (let i = 1; 1 <= 100; 1++) {
-   document.write(i + "<br>") 
-}
+// for (let i = 1; 1 <= 100; 1++) {
+//    document.write(i + "<br>") 
+// }
 
 /* 
 start point i = 1
@@ -17,8 +17,52 @@ i = 100
 conditions 100 <= 100 true (we go inside the loop (block)) 100 + <br>
 i = 101
 */
-let students = ["John", "test 2", "Mary", "Serri", "MJ", "Dusan"];
+// let students = ["John", "test 2", "Mary", "Serri", "MJ", "Dusan"];
 
-for (let i = 0; 1 < students.length, i++) {
-    document.write(students[i] + "<hr>");
+// for (let i = 0; 1 < students.length, i++) {
+//     document.write(students[i] + "<hr>");
+// }
+
+// standard for loop, <array, counting numbers>
+// for of loop user only with arrays and it print you vaalues
+// let students = [
+//     "John",
+//     "test 2",
+//     "Mary",
+//     "Serri",
+//     "MJ",
+//     "Dusan",
+//     "Johnny",
+//     "Karmin",
+// ];
+
+// students.forEach(function (val, i) {
+//     console.log(val, i);
+// }
+// );
+
+// students.forEach((element, i) => {
+//     console.log(element, i);
+// });
+
+
+let numbers = [50,90, 20, 99, 5];
+
+let max = numbers[0];
+
+for(let val of numbers) {
+    if(val > max){
+        max = val;
+    }
 }
+
+console.log(max);
+
+
+
+
+
+
+
+
+
